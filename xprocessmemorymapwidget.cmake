460194f372c64ba4d27ddfcd946f6104d1ee4741
horsicq <horsicq@gmail.com>
2024-03-01 00:08:18 +0100

    Update file(s): 2024-03-01

--- xprocessmemorymapwidget.cmake
+++ xprocessmemorymapwidget.cmake
@@ -5,7 +5,9 @@ include(${CMAKE_CURRENT_LIST_DIR}/../XProcess/xprocess.cmake)
 set(XPROCESSMEMORYMAPWIDGET_SOURCES
     ${XPROCESS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogxprocessmemorymap.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxprocessmemorymap.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogxprocessmemorymap.ui
     ${CMAKE_CURRENT_LIST_DIR}/xprocessmemorymapwidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xprocessmemorymapwidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xprocessmemorymapwidget.ui
 )
